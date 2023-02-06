b5147f787d628f85f6eb11bd1d70791b039d63a8
Jehe6447 <114559763+Jehe6447@users.noreply.github.com>
2023-02-05 17:24:43 -0700

    Create helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,14 @@
+// CSCI 1300 Fall 2022
+// Author: Jeremiah Heredia
+// Recitation: 102  – Tuan Tran
+// Homework 2 - Problem # 1
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
