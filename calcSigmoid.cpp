be28ad768b84f4edf8e3903d884f1b39386a13ba
Jehe6447 <114559763+Jehe6447@users.noreply.github.com>
2023-02-05 17:23:22 -0700

    Create calcSigmoid.cpp

---
+++ calcSigmoid.cpp
@@ -0,0 +1,18 @@
+// CSCI 1300 Fall 2022
+// Author: Jeremiah Heredia
+// Recitation: 102  – Tuan Tran
+// Homework 2 - Problem # 6
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+    double x;
+    double sig;
+    cout << "Enter a value for x:"<< endl;
+    cin >> x;
+    sig = 1/(1+exp(-x));
+    cout << "The sigmoid for x=" << x << " is " << sig;
+    return 0;
+}
