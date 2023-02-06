dc179508c39f8cf5c6e51d538258f0a29392c3d7
Jehe6447 <114559763+Jehe6447@users.noreply.github.com>
2023-02-05 17:18:51 -0700

    Create almondMilk.cpp

---
+++ almondMilk.cpp
@@ -0,0 +1,20 @@
+// CSCI 1300 Fall 2022
+// Author: Jeremiah Heredia
+// Recitation: 102  – Tuan Tran
+// Homework 2 - Problem # 3
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+  double length,height,ounces,cubicinches;
+    cout << "What is the side length of the base of the carton in inches?"<< endl;
+    cin >> length;
+    cout << "What is the height of the carton in inches?" << endl;
+    cin >> height;
+    cubicinches = length*length*height;
+    ounces = 0.55*cubicinches;
+    cout << "The carton has a volume of " <<setprecision(1) << fixed << ounces << " ounces." << endl;
+    return 0;
+}
