ec74ff8a27e62cc2d65a0aca5009c4719b4da63a
Jehe6447 <114559763+Jehe6447@users.noreply.github.com>
2023-02-05 17:25:14 -0700

    Create helloYou.cpp

---
+++ helloYou.cpp
@@ -0,0 +1,19 @@
+// CSCI 1300 Fall 2022
+// Author: Jeremiah Heredia
+// Recitation: 102  – Tuan Tran
+// Homework 2 - Problem # 2
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //declare a character array to hold user name
+    char name [50];
+    // print message to user to enter name
+    cout << "Please enter your name below:" << endl;
+    // get input from user to name []
+    cin >> name;
+    // print the name back with Hello
+    cout << "Hello, " << name << "!" << endl;
+    return 0;
+}
