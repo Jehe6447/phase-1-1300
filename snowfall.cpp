d46baa1c380781cfef682a7f22607d34ba1c1bfa
Jehe6447 <114559763+Jehe6447@users.noreply.github.com>
2023-02-05 17:26:02 -0700

    Create snowfall.cpp

---
+++ snowfall.cpp
@@ -0,0 +1,36 @@
+// CSCI 1300 Fall 2022
+// Author: Jeremiah Heredia
+// Recitation: 102  – Tuan Tran
+// Homework 2 - Problem # 4
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // initial snow depth at each city
+    int snowBreckenridge = 25;
+    int snowVail = 28;
+    int snowCopperMountain = 40;
+    
+    // snowfall rates and melt rates for each city
+    int snowfallRateBreckenridge = 10, meltRateBreckenridge= 5;
+    int snowfallRateVail = 14, meltRateVail = 2;
+    int snowfallRateCopperMountain = 5, meltRateCopperMountain = 3;
+   
+    // input the number of days
+    cout << "How many days in the future would you like a prediction for? " <<endl;
+    int days; 
+    cin >> days;
+    
+    // update the snow depth, keeping in mind that snow depth can't be negative
+    int updatedSnowBreckenridge = snowBreckenridge + days*(snowfallRateBreckenridge-meltRateBreckenridge);
+    int updatedSnowVail = snowVail+ days*(snowfallRateVail-meltRateVail);
+    int updatedSnowCopperMountain = snowCopperMountain + days* (snowfallRateCopperMountain - meltRateCopperMountain);
+    
+    //print the snow depths for each city
+    cout << "Breckenridge will have " << updatedSnowBreckenridge << " inches, ";
+    cout << "Vail will have " <<updatedSnowVail << " inches, ";
+    cout <<"and Copper Mountain will have " << updatedSnowCopperMountain << " inches.";
+    return 0;
+}
